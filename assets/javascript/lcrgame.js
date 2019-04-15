d6a158f8c1a0b329d771fdd0c9e455a663a2d218
golemheavy <steve@gridtop.com>
2019-04-15 16:37:18 -0500

    improved input control for receiving number of players

--- assets/javascript/lcrgame.js
+++ assets/javascript/lcrgame.js
@@ -1,5 +1,5 @@
 const gamestatus1 = document.getElementById("gamestatus1");
-const gamestatus2 = document.getElementById("gamestatus2");
+var gamestatus2 = document.getElementById("gamestatus2");
 
 /*
 gamestatus1.textContent = "";
@@ -16,12 +16,28 @@ const game = {
 }
 
 function initializeGame(gameobj) {
-	gamestatus1.textContent = "How many players?";
+	var enteredPlayers = null;
+	gamestatus1.textContent = "How many players? (For 3 to 20 players)";
 	gamestatus2.textContent = "";
 	
 	document.onkeyup = function(event) {
 		var pressedkey = event.key;
-		if ("1234567890".includes(pressedkey)) gamestatus2.textContent += pressedkey;
+		var numString = "";
+		if ("1234567890".includes(pressedkey)) {
+			if(gamestatus2.textContent.length < 2) gamestatus2.textContent += pressedkey;
+			else if (gamestatus2.textContent.length === 2) {
+				numString = gamestatus2.textContent.charAt(1) + pressedkey;
+				gamestatus2.textContent = numString;
+			}
+		}
+		if (event.which === 13) {
+			enteredPlayers = parseInt(gamestatus2.textContent);
+			console.log(enteredPlayers);
+			if (enteredPlayers > 2 && enteredPlayers < 21) { console.log("good number of players");} // good number of players
+			else {
+				gamestatus1.textContent = "please choose a number of players from 3 to 20."; gamestatus2.textContent = "";
+			}
+		}
 	}
 }
 
