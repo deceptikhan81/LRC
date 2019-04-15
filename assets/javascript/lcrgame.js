8b1e6f5a1e54cd0c1b26f689c1046b5d2c64143b
golemheavy <steve@gridtop.com>
2019-04-15 15:46:25 -0500

    basic interface included

--- assets/javascript/lcrgame.js
+++ assets/javascript/lcrgame.js
@@ -1,3 +1,32 @@
-footer {
-	width: 100%;
+const gamestatus1 = document.getElementById("gamestatus1");
+const gamestatus2 = document.getElementById("gamestatus2");
+
+/*
+gamestatus1.textContent = "";
+gamestatus2.textContent = "";
+console.log(gamestatus1);
+console.log(gamestatus2);
+*/
+
+const player = {};
+
+const game = {
+	players		:	[],
+	over		:	false
 }
+
+function initializeGame(gameobj) {
+	gamestatus1.textContent = "How many players?";
+	gamestatus2.textContent = "";
+	
+	document.onkeyup = function(event) {
+		var pressedkey = event.key;
+		if ("1234567890".includes(pressedkey)) gamestatus2.textContent += pressedkey;
+	}
+}
+
+function play(gameobj) {
+}
+	
+initializeGame(game);
+play(game);
